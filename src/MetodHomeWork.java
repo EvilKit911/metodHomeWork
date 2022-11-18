0b08068406bc8685911eda8595311230f455d2d5
Nikita <19nikita98.3939@gmail.com>
2022-11-18 19:49:39 +0200

    задание 1

---
+++ src/MetodHomeWork.java
@@ -0,0 +1,26 @@
+public class MetodHomeWork {
+
+    public static void main(String[] args) {
+        // задание 1
+    int testYear = 2020;
+    checkYear(testYear); // передача значения в метод проверки кода( не понимаю как это работает)
+
+}
+        //  задание 1
+    public static int checkYear (int testYear){ // метод проверки года, принимает значение года
+
+
+        if(testYear % 4 == 0 & testYear % 100 != 0){
+            // если год делиться на 4 без остатка и делиться на 100 с остатком год високосный
+            System.out.println(testYear + " год високосный");
+        }else if (testYear % 4 ==0 & testYear % 100 ==0 & testYear % 400 == 0){
+            // иначе если год делиться на 4 на 100 и на 400 без остатка
+            System.out.println(testYear + " год високосный");
+        } else System.out.println(testYear + " год не високосный");
+        // иначе год не високосный
+
+        return testYear;// возвращаем значение
+    }
+
+
+}
