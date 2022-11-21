4db966fc057806f0ae9ac1855ca235a93d8e2795
Nikita <19nikita98.3939@gmail.com>
2022-11-21 13:06:44 +0200

    Задание 2

--- src/MetodHomeWork.java
+++ src/MetodHomeWork.java
@@ -5,6 +5,12 @@ public class MetodHomeWork {
 // задание 1
         int testYear = 2023;
         checkYear(testYear); // передача значения в метод проверки кода( не понимаю как это работает)
+
+        // задание 2
+        int currentYear = LocalDate.now().getYear();// текущая дата (скоро 23 год УРА! УРА!)
+        int clientOS = 0;// клинетская ось 0 - IOS, 1 - Android
+        int clientDeviceYear = 2022;// год выхода телефона
+        checkPhoneAndSetupBankApp(currentYear, clientOS, clientDeviceYear);
     }
     //  задание 1
     public static int checkYear (int testYear){ // метод проверки года, принимает значение года
@@ -20,5 +26,22 @@ public class MetodHomeWork {
         // иначе год не високосный
 
         return testYear;// возвращаем значение
+    }
+
+    //задание 2
+    public static int checkPhoneAndSetupBankApp(int currentYear, int clientOS, int clientDeviceYear) {
+        System.out.println(" ");
+
+        if (clientOS == 0 & clientDeviceYear >= currentYear) {
+            System.out.println("Установите версию приложения для iOS по ссылке");
+        } else if (clientOS == 0 & clientDeviceYear < currentYear) {
+            System.out.println("Установите облегченную версию приложения для iOS по ссылке");
+        } else if (clientOS == 1 & clientDeviceYear >= currentYear) {
+            System.out.println("Установите версию приложения для Android по ссылке");
+        } else if (clientOS == 1 & clientDeviceYear < currentYear) {
+            System.out.println("Установите облегченную версию приложения для Android по ссылке");
+        } else System.out.println(" Такой версии OC нет ");
+
+        return currentYear & clientOS & clientDeviceYear;
     }
 }
